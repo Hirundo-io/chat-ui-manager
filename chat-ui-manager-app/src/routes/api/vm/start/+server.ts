f27c71dfb8c6325130fb3cae7056b820772aba38
Tamir <tamir@hirundo.io>
2025-03-27 14:01:16 +0200

    Try Switching to `ManagedIdentityCredential` from `AzureCliCredential` for production environment

--- chat-ui-manager-app/src/routes/api/vm/start/+server.ts
+++ chat-ui-manager-app/src/routes/api/vm/start/+server.ts
@@ -1,13 +1,13 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import { ComputeManagementClient } from '@azure/arm-compute';
-import { AzureCliCredential } from '@azure/identity';
+import { ManagedIdentityCredential } from '@azure/identity';
 import { MachineStatus } from '$lib/types';
 
 const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID || '';
 const resourceGroupName = process.env.AZURE_RESOURCE_GROUP || '';
 const vmName = process.env.AZURE_VM_NAME || '';
-const credential = new AzureCliCredential();
+const credential = new ManagedIdentityCredential();
 const computeClient = new ComputeManagementClient(credential, subscriptionId);
 
 export const POST: RequestHandler = async () => {
