0d8a5e54e9b6fd6a55ff8500d31adb6fd95dd158
Tamir <tamir@hirundo.io>
2025-03-27 17:19:03 +0200

    Use `AzureCliCredential` for local development and `ManagedIdentityCredential` for production

--- chat-ui-manager-app/src/routes/api/vm/start/+server.ts
+++ chat-ui-manager-app/src/routes/api/vm/start/+server.ts
@@ -1,13 +1,17 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import { ComputeManagementClient } from '@azure/arm-compute';
-import { ManagedIdentityCredential } from '@azure/identity';
+import { AzureCliCredential, ManagedIdentityCredential } from '@azure/identity';
 import { MachineStatus } from '$lib/types';
 
 const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID || '';
 const resourceGroupName = process.env.AZURE_RESOURCE_GROUP || '';
 const vmName = process.env.AZURE_VM_NAME || '';
-const credential = new ManagedIdentityCredential();
+
+// Use AzureCliCredential for local development and ManagedIdentityCredential for Azure environment
+const credential = process.env.IS_LOCAL_ENV
+	? new AzureCliCredential()
+	: new ManagedIdentityCredential();
 const computeClient = new ComputeManagementClient(credential, subscriptionId);
 
 export const POST: RequestHandler = async () => {
